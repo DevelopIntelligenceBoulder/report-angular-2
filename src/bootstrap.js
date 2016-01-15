18ffef293e007dc9366173aca1669797a40d5c72
kamrenz <kamren@developintelligence.com>
2016-01-14 20:08:22 -0700

    Adding application HTML & Bootstrapping

---
+++ src/bootstrap.js
@@ -0,0 +1,4 @@
+var browser_1 = require("angular2/platform/browser");
+var report_app_js_1 = require("./components/report-app.js");
+//Bootstrapping main Angular Component
+browser_1.bootstrap(report_app_js_1.ReportApp);
---
+++ src/bootstrap.ts
@@ -0,0 +1,5 @@
+import { bootstrap } from "angular2/platform/browser";
+import { ReportApp } from "./components/report-app.js";
+
+//Bootstrapping main Angular Component
+bootstrap(ReportApp);
