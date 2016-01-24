a431aa7eae332f7982a2802894d70920bf75f326
kamrenz <kamren@developintelligence.com>
2016-01-24 08:26:06 -0700

    Updating boostrap to include HTTP

--- src/app/bootstrap.js
+++ src/app/bootstrap.js
@@ -1,16 +1,21 @@
-System.register(["angular2/platform/browser", "./components/report-app"], function(exports_1) {
-    var browser_1, report_app_1;
+/// <reference path="../../node_modules/angular2/bootstrap.d.ts" />
+/// <reference path="../../node_modules/angular2/http.d.ts" />
+System.register(["angular2/platform/browser", "angular2/http", "./components/report-app"], function(exports_1) {
+    var browser_1, http_1, report_app_1;
     return {
         setters:[
             function (browser_1_1) {
                 browser_1 = browser_1_1;
+            },
+            function (http_1_1) {
+                http_1 = http_1_1;
             },
             function (report_app_1_1) {
                 report_app_1 = report_app_1_1;
             }],
         execute: function() {
             //Bootstrapping main Angular Component
-            browser_1.bootstrap(report_app_1.ReportApp);
+            browser_1.bootstrap(report_app_1.ReportApp, [http_1.HTTP_PROVIDERS]);
         }
     }
 });
--- src/app/bootstrap.ts
+++ src/app/bootstrap.ts
@@ -1,6 +1,9 @@
 /// <reference path="../../node_modules/angular2/bootstrap.d.ts" />
+/// <reference path="../../node_modules/angular2/http.d.ts" />
+
 import { bootstrap } from "angular2/platform/browser";
+import { HTTP_PROVIDERS } from "angular2/http";
 import { ReportApp } from "./components/report-app";
 
 //Bootstrapping main Angular Component
-bootstrap(ReportApp);
+bootstrap(ReportApp, [ HTTP_PROVIDERS ]);
